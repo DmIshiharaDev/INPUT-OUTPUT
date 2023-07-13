dcc5847c2e8d9f78fcfb4e2561f8ce0e469072b3
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-07-13 14:05:40 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio03-operadores/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("- - - - - Digite uma frase abaixo - - - - -");
+        Console.Write("Frase: ");
+        string frase = Console.ReadLine();
+
+        Console.WriteLine("Resultados:");
+        Console.WriteLine("Frase: " + frase);
+        Console.ReadLine();
+    }
+}
